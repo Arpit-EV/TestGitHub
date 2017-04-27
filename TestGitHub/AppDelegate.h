2795358a9e998df4e955e2c24c6f6946f9ddf4e4
Arpit Sharma <Arpit@Arpits-MacBook-Pro.local>
2017-04-27 17:23:55 +0530

    Initial Commit

---
+++ TestGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitHub
+//
+//  Created by Arpit Sharma on 27/04/17.
+//  Copyright © 2017 Edutainment Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGitHub/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestGitHub
+//
+//  Created by Arpit Sharma on 27/04/17.
+//  Copyright © 2017 Edutainment Ventures. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
