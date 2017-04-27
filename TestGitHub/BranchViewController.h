7e877f9313b06aaee98f3db112afdf88eaa33b37
Ranjeet <ranjeet@ranjeetmac.local>
2017-04-27 17:33:01 +0530

    initial commit

---
+++ TestGitHub/BranchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BranchViewController.h
+//  TestGitHub
+//
+//  Created by Ranjeet on 4/27/17.
+//  Copyright © 2017 Edutainment Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BranchViewController : UIViewController
+
+@end
---
+++ TestGitHub/BranchViewController.m
@@ -0,0 +1,37 @@
+//
+//  BranchViewController.m
+//  TestGitHub
+//
+//  Created by Ranjeet on 4/27/17.
+//  Copyright © 2017 Edutainment Ventures. All rights reserved.
+//
+
+#import "BranchViewController.h"
+
+@interface BranchViewController ()
+
+@end
+
+@implementation BranchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
